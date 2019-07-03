f00fdcba83e8b14c78ad599cce85c102bf9cd3de
Ian Blackstone <ianblackstone@gmail.com>
2019-07-03 14:53:20 -0400

    Initial versions of BinnedAnalyzer.h and .cc.  Updated to do document.
    An initial work in progress version of the BinnedAnalyzer code.

---
+++ include/BinnedAnalyzer.h
@@ -0,0 +1,17 @@
+#ifndef UCORRELATOR_BINNED_ANALYZER_H
+#define UCORRELATOR_BINNED_ANALYZER_H
+
+#include Analyzer.h
+#include BinnedCorrelator.h
+#include BinnedAnitaEventSummary.h
+
+// This class inherits the properties of AnalysisConfig from AnalysisConfig.h but adds options for normalization calls inside the Binned Analysis code.
+// Any calls in the BA code AnalysisConfig should point instead to BinnedAnalysisConfig.
+class BinnedAnalyzer : public Analyzer{
+
+	private:
+		BinnedCorrelator bcorr;
+  
+}
+
+#endif

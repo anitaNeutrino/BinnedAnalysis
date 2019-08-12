3fcbcc606b859f5c1135dd61c09710ff9eb36976
Ian Blackstone <ianblackstone@users.noreply.github.com>
2019-08-12 15:01:41 -0400

    Creating CmakeLists.txt and LinkDef.h
    Creating initial versions of the files needed for CMAKE, currently non functional.

---
+++ LinkDef.h
@@ -0,0 +1,26 @@
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+#pragma link C++ nestedclasses;
+
+
+//#pragma link C++ class RadioScatter+;
+
+
+//#pragma link C++ namespace CLHEP;
+//#pragma link C++ class Hep3Vector+;
+//#pragma link C++ class HepLorentzVector+;
+
+//#pragma link C++ class 
+
+//#pragma link C++ defined_in "/usr/local/include/CLHEP/Vector/defs.h";
+//#pragma link C++ defined_in "/usr/local/include/CLHEP/Vector/ThreeVector.h";
+#pragma link C++ class BinnedAnalyzer+;
+#pragma link C++ class BinnedAnitaEventSummary+;
+#pragma link C++ class BinnedBedmapReader+;
+#pragma link C++ class BinnedCorrelator+;
+#pragma link C++ class Normalization+;
+
+#endif
